9b5dc18548fdae78bfbf25cee0ae19c858aedfd1
Anushkothuri <kothurianush16@gmail.com>
2024-04-14 20:06:51 +0530

    for loop

---
+++ problem2.c
@@ -0,0 +1,13 @@
+// Print Natural numbers from User "K value to 1".
+#include<stdio.h>
+int main()
+{
+    int K,i;
+    printf("enter K value:");
+    scanf("%d",&K);
+    for(i=K;i>=1;i--)
+    {
+        printf("%d\t",i);
+    }
+    return 0;
+}
